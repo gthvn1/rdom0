ea83e4e0e0f3c3eedd81e50271540cc16c2348fe
Guillaume <guillaume.thouvenin@vates.tech>
2024-05-11 21:09:27 +0200

    Remove the need of std
    - We need to add panic_handler
    - Unwinding is not supported without std so just abort
    - use no main with no mangle as we are not using std

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 Work is in progress...
 
+- Currently we are trying to build freestanding rust binary:
+    - build: `cargo rustc -- -C link-arg=-nostartfiles`
+
 ## Links
 
 [Writing an OS in Rust](https://os.phil-opp.com)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+/// This function is called on panic and is required to build.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+// use C calling convention
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
 }
